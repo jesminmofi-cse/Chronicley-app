7a156be1fec2c1b54a8fcf78c71ddd7884f159a0
Jesmin Mofi S <162531919+jesminmofi-cse@users.noreply.github.com>
2025-12-19 14:03:46 +0530

    update

--- frontend/src/Auth/LoginPage.jsx
+++ frontend/src/Auth/LoginPage.jsx
@@ -26,7 +26,7 @@ const LoginPage = () => {
 
     try {
       const res = await axios.post(
-        'http://localhost:5000/api/auth/login',
+        'http://localhost:5000/api/auth/login','https://chronicley-app.onrender.com/api/auth/login'
         formData,
         { headers: { 'Content-Type': 'application/json' } }
       );
