5ed68d2016b0e3eb5d91719d9bfc81976a56eb41
Jesmin Mofi S <162531919+jesminmofi-cse@users.noreply.github.com>
2025-12-19 13:57:45 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 //  CORS
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000','https://chronicley-app.onrender.com'
   credentials: true
 }));
 
