045f5ba2873b108f1c0cde9f13b3af6cd224db48
Jesmin Mofi S <162531919+jesminmofi-cse@users.noreply.github.com>
2025-12-21 20:34:45 +0530

    update

--- frontend/src/Planner/Layout.jsx
+++ frontend/src/Planner/Layout.jsx
@@ -1,15 +1,17 @@
-import React, { Children } from 'react';
+// src/components/Layout.jsx
+import React from 'react';
 import Sidebar from './Sidebar';
 import './Layout.css';
-const Layout=({Children})=>{
-    return (
-        <div className='layout-container'>
-            <Sidebar/>
-            <main classNAme='main-content'>
-                {Children}
-            </main>
-        </div>
 
-    );
+const Layout = ({ children }) => {
+  return (
+    <div className="layout-container">
+      <Sidebar />
+      <main className="main-content">
+        {children}
+      </main>
+    </div>
+  );
 };
+
 export default Layout;
